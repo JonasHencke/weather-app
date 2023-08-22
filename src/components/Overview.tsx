c31193006cf4ba4b9669ab9c75f4d6be9b6e5eb5
jonas-1997 <jvhencke@googlemail.com>
2023-08-22 17:52:26 +0200

    Add elements and styling to Overview component

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,5 +1,18 @@
 import React from "react"
+import icon from "../SVGs/thunder.svg"
+import cloud from "../SVGs/cloudy.svg"
+import rain from "../SVGs/rain-svgrepo-com.svg"
 
 export default function Overview() {
-    return (<div className="overview-component"></div>)
+    return (
+    <div className="overview-component">
+        <div className="searchbar">Search</div>
+        <img className="overview-image" src ={icon}/>
+        <p className="overview-temperature">16°C</p>
+        <p className="overview-date">Montag, 16:00</p>
+        <p className="overview-wrapper"><img src={cloud}/>leicht bewölkt</p>
+        <p className="overview-wrapper"><img src={rain}/>Regenwahrscheinlichkeit: <br/> 30% </p>
+        <div className="overview-location">Leipzig, Sachsen, Deutschland</div>
+    </div>
+    )
 }
