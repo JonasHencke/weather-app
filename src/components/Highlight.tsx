6c1742660404d4e4ef46fe7398800a8b7418f182
jonas-1997 <jvhencke@googlemail.com>
2023-08-25 18:57:42 +0200

    Display API Data in children components with useState and useContext

--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useContext } from "react"
+import { WeatherContext } from "./Layout";
 
 export default function Highlight() {
+  const weatherData = useContext(WeatherContext)
+
   return (
     <div className="highlight-component">
-      <div className="highlight"> UV-Index 6</div>
+      <div className="highlight">{weatherData ? weatherData.coord.lon : null}</div>
       <div className="highlight">Wind</div>
       <div className="highlight">Sonenaufgang & Sonnenuntergang</div>
       <div className="highlight">Luftfeuchtigkeit</div>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,30 +1,36 @@
-import React, {useState, useEffect} from "react"
-import Overview from "./Overview"
-import Header from "./Header"
-import Week from "./week"
-import Highlight from "./Highlight"
-import example from "../example.json"
-import getWeather from "../utils/GetWeatherData"
+import React, { useState, useEffect, createContext } from "react";
+import Overview from "./Overview";
+import Header from "./Header";
+import Week from "./week";
+import Highlight from "./Highlight";
+import example from "../example.json";
+import getWeather from "../utils/getWeatherData";
+
+export const WeatherContext = createContext(null);
 
 export default function Layout() {
-    type Weather = null | object
-    const [weatherData, setWeatherData] = useState<Weather>(null)
+  type Weather = (null | object)
 
-    useEffect(() => {
-        setWeatherData(getWeather("berlin"))
-        console.log(weatherData)
-    }, [])
-    return (
-    <div className="flex-layout">
-    <div className="flex-left">
-        <Overview/>
-    </div>
-    <div className="flex-right">
-        <Header/>
-        <Week/>
-        <Highlight/>
-    </div>
-    </div>
-    )
+  const [weatherData, setWeatherData] = useState<Weather>(null);
+
+  useEffect(() => {
+    getWeather("berlin").then(data => (
+      setWeatherData(data)
+    ))
+  }, []);
+  
+  return (
+    <WeatherContext.Provider value={weatherData}>
+      <div className="flex-layout">
+        <div className="flex-left">
+          <Overview />
+        </div>
+        <div className="flex-right">
+          <Header />
+          <Week />
+          <Highlight />
+        </div>
+      </div>
+    </WeatherContext.Provider>
+  );
 }
-
