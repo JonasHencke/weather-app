87c175ea9746e491d0588f341187e48e874e168e
jonas-1997 <jvhencke@googlemail.com>
2023-08-31 16:35:35 +0200

    Implement calculateTemperature function into Hour component

--- src/components/Hour.tsx
+++ src/components/Hour.tsx
@@ -12,6 +12,7 @@ import thunder from "../SVGs/thunder.svg";
 import snow from "../SVGs/snowy-6.svg";
 import night from "../SVGs/night.svg";
 import cloudyNight from "../SVGs/cloudy-night-3.svg";
+import calculateTemperature from "../utils/calculateTemperature";
 
 function pickIcon(id: string) {
   if (id == "01d") {
@@ -36,44 +37,44 @@ function pickIcon(id: string) {
 }
 
 export default function Week() {
-  const { weatherData, ForecastData } =
-    useContext<WeatherContextType | null>(WeatherContext);
+  const { ForecastData, temperatureUnit } =
+    useContext<WeatherContextType>(WeatherContext);
   return (
     <div className="week-component">
       <div className="weekday">
         {ForecastData ? getTime(ForecastData.hourly[1].dt, ForecastData.timezone).slice(0,2) : null} Uhr
         <img src={ForecastData ? pickIcon(ForecastData.hourly[1].weather[0].icon) : null} />
-        <p>{ForecastData ? Math.floor(ForecastData.hourly[1].temp - 273.15) : null}°C</p>
+        <p>{ForecastData ? calculateTemperature(temperatureUnit , ForecastData.hourly[1].temp) : null}</p>
       </div>
       <div className="weekday">
         {ForecastData ? getTime(ForecastData.hourly[2].dt, ForecastData.timezone).slice(0,2) : null} Uhr
         <img src={ForecastData ? pickIcon(ForecastData.hourly[2].weather[0].icon) : null} />
-        <p>{ForecastData ? Math.floor(ForecastData.hourly[2].temp - 273.15) : null}°C</p>
+        <p>{ForecastData ? calculateTemperature(temperatureUnit , ForecastData.hourly[2].temp) : null}</p>
       </div>
       <div className="weekday">
         {ForecastData ? getTime(ForecastData.hourly[3].dt, ForecastData.timezone).slice(0,2) : null} Uhr
         <img src={ForecastData ? pickIcon(ForecastData.hourly[3].weather[0].icon) : null} />
-        <p>{ForecastData ? Math.floor(ForecastData.hourly[3].temp - 273.15) : null}°C</p>
+        <p>{ForecastData ? calculateTemperature(temperatureUnit , ForecastData.hourly[3].temp) : null}</p>
       </div>
       <div className="weekday">
         {ForecastData ? getTime(ForecastData.hourly[4].dt, ForecastData.timezone).slice(0,2) : null} Uhr
         <img src={ForecastData ? pickIcon(ForecastData.hourly[4].weather[0].icon) : null} />
-        <p>{ForecastData ? Math.floor(ForecastData.hourly[4].temp - 273.15) : null}°C</p>
+        <p>{ForecastData ? calculateTemperature(temperatureUnit , ForecastData.hourly[4].temp) : null}</p>
       </div>
       <div className="weekday">
         {ForecastData ? getTime(ForecastData.hourly[5].dt, ForecastData.timezone).slice(0,2) : null} Uhr
         <img src={ForecastData ? pickIcon(ForecastData.hourly[5].weather[0].icon) : null} />
-        <p>{ForecastData ? Math.floor(ForecastData.hourly[5].temp - 273.15) : null}°C</p>
+        <p>{ForecastData ? calculateTemperature(temperatureUnit , ForecastData.hourly[5].temp) : null}</p>
       </div>
       <div className="weekday">
         {ForecastData ? getTime(ForecastData.hourly[6].dt, ForecastData.timezone).slice(0,2) : null} Uhr
         <img src={ForecastData ? pickIcon(ForecastData.hourly[6].weather[0].icon) : null} />
-        <p>{ForecastData ? Math.floor(ForecastData.hourly[6].temp - 273.15) : null}°C</p>
+        <p>{ForecastData ? calculateTemperature(temperatureUnit , ForecastData.hourly[6].temp) : null}</p>
       </div>
       <div className="weekday">
         {ForecastData ? getTime(ForecastData.hourly[7].dt, ForecastData.timezone).slice(0,2) : null} Uhr
         <img src={ForecastData ? pickIcon(ForecastData.hourly[7].weather[0].icon) : null} />
-        <p>{ForecastData ? Math.floor(ForecastData.hourly[7].temp - 273.15) : null}°C</p>
+        <p>{ForecastData ? calculateTemperature(temperatureUnit , ForecastData.hourly[7].temp) : null}</p>
       </div>
     </div>
   );
