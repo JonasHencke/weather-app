ba90140bdbef26be4f74b41a531d6aa7b1be5023
jonas-1997 <jvhencke@googlemail.com>
2023-09-02 20:28:18 +0200

    Add try and catch to getCityImage function

--- src/utils/getCityImage.tsx
+++ src/utils/getCityImage.tsx
@@ -1,5 +1,9 @@
 export default async function getCityImage(city: string) {
     const response = await fetch(`https://api.teleport.org/api/urban_areas/slug:${city}/images/`);
     const images = await response.json();
-    return images
+    try {
+        return images;
+      } catch {
+        return null
+      }
  }
--- src/utils/getLocation.tsx
+++ src/utils/getLocation.tsx
@@ -1,5 +1,5 @@
 export default async function getLocation(city: string) {
     const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=20f7632ffc2c022654e4093c6947b4f4`);
-    const weather = await response.json();
-      return weather
+    const location = await response.json();
+      return location
  }
